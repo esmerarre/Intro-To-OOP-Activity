37c730becfa52c3c0a28d683280473a8596a8280
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-09-25 21:02:28 -0700

    completed classwork

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,8 @@
 # add your get_student_with_more_classes function here!
+def get_student_with_more_classes(first_student, second_student):
+    if first_student.get_num_classes() > second_student.get_num_classes():
+        return first_student.name
+    elif first_student.get_num_classes() == second_student.get_num_classes():
+        return None
+    return second_student.name
+    
--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,16 @@
-# add your Student class here!
+class Student:
+    def __init__(self, name, grade, classes):
+        self.name = name
+        self.grade = grade
+        self.classes = classes
+
+    def add_class(self, new_class):
+        self.classes.append(new_class)
+        return self.classes
+    
+    def get_num_classes(self):
+        return len(self.classes)
+    
+    def summary(self):
+        num_classes = self.get_num_classes()
+        print(f"{self.name} is a {self.grade} enrolled in {num_classes} classes")
